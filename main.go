4bc6b9b1580d57ba608d1ecd3ba80001bdfa5c7c
Billy Lam <0chun0@gmail.com>
2021-02-28 18:18:48 +0800

    w/e

--- main.go
+++ main.go
@@ -2,110 +2,35 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"time"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type Quandl struct {
-	token     string
-	limit     int
-	startDate string // e.g. 2019-01-01
-	endDate   string
-	order     string
+type Industry struct {
+	Group     string
+	Industry  string
+	CodeF     string
+	Close     float64
+	Change    float64
+	ChangePct float64
+	Volume    int
+	Turnover  int
+	PE        float64 // Price per Earnings
+	PB        float64 // Price to Book
+	YieldPct  float64
+	MarketCap int
 }
-
-type option func(*Quandl)
 
 func main() {
-
-	quandl := New()
-	quandl.Option(SetLimit(10))
-	quandl.Option(SetEndDate("2021-01-02"))
-	quandl.Option(SetOrder("desc"))
-
-	result, err := quandl.GetHistoricalPrice(5)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(PrettyPrint(result))
-}
-
-type HistoricalPrice struct {
-	DatasetData struct {
-		Limit       interface{}     `json:"limit"`
-		Transform   interface{}     `json:"transform"`
-		ColumnIndex interface{}     `json:"column_index"`
-		ColumnNames []string        `json:"column_names"`
-		StartDate   string          `json:"start_date"`
-		EndDate     string          `json:"end_date"`
-		Frequency   string          `json:"frequency"`
-		Data        [][]interface{} `json:"data"`
-		Collapse    interface{}     `json:"collapse"`
-		Order       string          `json:"order"`
-	} `json:"dataset_data"`
-}
-
-func New() Quandl {
-
-	token, err := getQuanToken()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	return Quandl{
-		limit:     100,
-		token:     token,
-		startDate: "2015-01-01",                    // Hard code for now, as there is a row limit for api call
-		endDate:   time.Now().Format("2006-01-02"), // Default for today, in yyyy-mm-dd format
-		order:     "desc",
-	}
-}
-
-func (q *Quandl) GetHistoricalPrice(code int) (HistoricalPrice, error) {
-	var data HistoricalPrice
-	c := fmt.Sprintf("%05d", code)
-	endpoint, err := q.getQuanEndPoint("HKEX", c, "historicalPrice")
-	resp, err := http.Get(endpoint)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return data, err
-	}
-
-	if err := json.Unmarshal(body, &data); err != nil {
-		panic(err)
-	}
-
-	return data, nil
-}
-
-// getQuanEndPoint returns the endpoint of the api callOB
-func (q *Quandl) getQuanEndPoint(db, code, api string) (string, error) {
-	var endpoint string
-	switch api {
-	case "historicalPrice":
-		endpoint = "https://www.quandl.com/api/v3/datasets/HKEX/00005/data.json?api_key=%s&start_date=%s&end_date=%s&order=%s&limit=%d"
-	default:
-		return "", fmt.Errorf("no api endpoint - &s", api)
-	}
-	endpoint = fmt.Sprintf(endpoint, q.token, q.startDate, q.endDate, q.order, q.limit)
-
-	return endpoint, nil
-}
-
-// getQuanToken returns the
-func getQuanToken() (string, error) {
-	token, ok := os.LookupEnv("QUANDL_TOKEN")
-	if !ok {
-		return token, errors.New("quandl token not set, please check your env variable QUANDL_TOKEN")
-	}
-	return token, nil
+	// GetIndustryDetails()
+	dev()
 }
 
 // PrettyPrint to print struct in a readable way
@@ -114,37 +39,191 @@ func PrettyPrint(i interface{}) string {
 	return string(s)
 }
 
-// Option sets the options specified.
-func (q *Quandl) Option(opts ...option) {
-	for _, opt := range opts {
-		opt(q)
+func GetIndustryDetails() {
+	links := getLinks()
+	var results []Industry
+
+	for _, link := range links {
+		industry, _ := getDetails(link)
+		results = append(results, industry...)
 	}
+
+	fmt.Println(PrettyPrint(results))
+	fmt.Println(len(results))
+
 }
 
-// SetLimit sets Foo's verbosity level to v.
-func SetLimit(v int) option {
-	return func(q *Quandl) {
-		q.limit = v
+func getDetails(link string) ([]Industry, error) {
+
+	var result []Industry
+	fmt.Printf("Getting link - %s\n", link)
+
+	res, err := http.Get(link)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Title
+	var (
+		group     string // Higher group, e.g. Materials
+		industry  string // Industry, e.g. Chemical Products
+		code      string
+		close     float64
+		change    float64
+		changePct float64
+		volume    int
+		turnover  int
+		pe        float64
+		pb        float64
+		yield     float64
+		marketCap int
+	)
+	doc.Find("h1").Each(func(i int, s *goquery.Selection) {
+		text := s.Text() // e.g. Industry Details - Materials - Chemical Products
+		texts := strings.Split(text, "-")
+		if len(texts) == 3 {
+			group = strings.TrimSpace(texts[1])    // e.g. Materials
+			industry = strings.TrimSpace(texts[2]) // e.g. Chemical Products
+		}
+	})
+
+	// Links
+	doc.Find("span.float_l").Each(func(i int, s *goquery.Selection) {
+		code = strings.TrimSpace(s.Text()) // e.g. 00301.HK
+		if strings.Contains(code, "0") {   // Check starts with 0
+			code = strings.ReplaceAll(code, ".HK", "") // 00301.HK -> 00301
+			ss := s.ParentsUntil("tbody")
+			var values []string
+
+			ss.Each(func(j int, tb *goquery.Selection) {
+				tb.Find("td.cls.txt_r.pad3").Each(func(i int, td *goquery.Selection) {
+					// fmt.Println(td.Text())
+					values = append(values, td.Text())
+				})
+			})
+
+			if len(values) == 10 {
+				_ = values[0] // Some empty string
+				close, _ = parseF(values[1])
+				change, _ = parseF(values[2])
+				changePct, _ = parseF(values[3])
+				volume, _ = parseI(values[4])
+				turnover, _ = parseI(values[5])
+				pe, _ = parseF(values[6])
+				pb, _ = parseF(values[7])
+				yield, _ = parseF(values[8])
+				marketCap, _ = parseI(values[9])
+
+				rec := Industry{
+					Group:     group,
+					Industry:  industry,
+					CodeF:     code,
+					Close:     close,
+					Change:    change,
+					ChangePct: changePct,
+					Volume:    volume,
+					Turnover:  turnover,
+					PE:        pe,
+					PB:        pb,
+					YieldPct:  yield,
+					MarketCap: marketCap,
+				}
+				result = append(result, rec)
+			}
+		}
+	})
+
+	return result, nil
 }
 
-// SetEndDate sets end date for the query in yyyy-mm-dd format
-func SetEndDate(v string) option {
-	return func(q *Quandl) {
-		q.endDate = v
+func getLinks() []string {
+	var links []string
+	res, err := http.Get("http://www.aastocks.com/en/stocks/market/industry/sector-industry-details.aspx")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+
+	r := regexp.MustCompile("gotoindustry\\(\\'(\\d{4})\\'\\)")
+	matches := r.FindAllStringSubmatch(string(body), -1)
+	for _, match := range matches {
+		if len(match) >= 2 {
+			industryCode := match[1]
+			link := fmt.Sprintf("http://www.aastocks.com/en/stocks/market/industry/sector-industry-details.aspx?industrysymbol=%s&t=1&s=&o=&p=", industryCode)
+
+			links = append(links, link)
+		}
+	}
+	return links
 }
 
-// SetStartDate sets start date for the query in yyyy-mm-dd format
-func SetStartDate(v string) option {
-	return func(q *Quandl) {
-		q.startDate = v
+func dev() {
+	res, err := http.Get("http://www.aastocks.com/en/stocks/market/industry/sector-industry-details.aspx?industrysymbol=2033&t=1&hk=0")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	re := regexp.MustCompile(`.*Last Update:\s*(\d{4}\/\d{2}\/\d{2})`)
+	matched := re.FindAllSubmatch(body, -1)
+
+	fmt.Println(string(matched[0][1]))
+
 }
 
-// SetOrder sets set the order of the query, e.g. asc/desc
-func SetOrder(v string) option {
-	return func(q *Quandl) {
-		q.order = v
+func parseF(s string) (float64, error) {
+	s = strings.ReplaceAll(s, "%", "")
+	num, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0.0, nil
 	}
+	return num, err
+}
+
+func parseI(s string) (int, error) {
+	var (
+		num int
+		f   float64
+		err error
+	)
+
+	if strings.Contains(s, "N/A") {
+		num = 0
+	}
+	if strings.Contains(s, "K") {
+		s = strings.ReplaceAll(s, "K", "")
+		f, err = strconv.ParseFloat(s, 64)
+		num = int(f * 1_000)
+	}
+	if strings.Contains(s, "M") {
+		s = strings.ReplaceAll(s, "M", "")
+		f, err = strconv.ParseFloat(s, 64)
+		num = int(f * 1_000_000)
+	}
+	if strings.Contains(s, "B") {
+		s = strings.ReplaceAll(s, "B", "")
+		f, err = strconv.ParseFloat(s, 64)
+		num = int(f * 1_000_000_000)
+	}
+	if err != nil {
+		return 0, err
+	}
+
+	return num, nil
 }
