c06699b43d3f64ec6167c0d629b8be1013a6c3fc
Billy Lam <0chun0@gmail.com>
2022-01-27 01:13:03 +0800

    chunk

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module example.com
 
 go 1.16
 
-require github.com/lib/pq v1.10.4
+require (
+	cloud.google.com/go v0.100.2
+	cloud.google.com/go/bigquery v1.27.0
+	github.com/lib/pq v1.10.4
+)
--- main.go
+++ main.go
@@ -1,38 +1,52 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
+	"cloud.google.com/go/bigquery"
+	"cloud.google.com/go/civil"
 	_ "github.com/lib/pq"
 )
 
 type HistoricalPrice struct {
-	Code     int     `csv:"-"`
-	CodeF    string  `csv:"-"` // code in string format
-	Date     string  `csv:"Date"`
-	Ask      float64 `csv:"Ask"`
-	Bid      float64 `csv:"Bid"`
-	Open     float64 `csv:"Previous Close"` // open is missing in quandl, using prev close
-	High     float64 `csv:"High"`
-	Low      float64 `csv:"Low"`
-	Close    float64 `csv:"Nominal Price"`
-	Volume   int     `csv:"Share Volume (000)"`
-	Turnover int     `csv:"Turnover (000)"`
+	Code     int        `bigquery:"code"`
+	CodeF    string     `bigquery:"codef"`
+	Date     civil.Date `bigquery:"date"`
+	Ask      float64    `bigquery:"ask"`
+	Bid      float64    `bigquery:"bid"`
+	Open     float64    `bigquery:"open"`
+	High     float64    `bigquery:"high"`
+	Low      float64    `bigquery:"low"`
+	Close    float64    `bigquery:"close"`
+	Volume   int        `bigquery:"volume"`
+	Turnover int        `bigquery:"turnover"`
 }
 
 func main() {
-	fmt.Println("main")
+	// fmt.Println("main")
 
-	records, err := GetRecords("stock")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(PrettyPrint(records))
+	// records, err := GetRecords("stock")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// fmt.Println(len(records))
 
+	// err = insertRows(records)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	in := makeRange(1, 29)
+	res := chunkSlice(in, 100)
+	fmt.Println(res)
 }
 
 func GetConnection() (*sql.DB, error) {
@@ -51,14 +65,14 @@ func GetConnection() (*sql.DB, error) {
 func GetRecords(table string) ([]HistoricalPrice, error) {
 	var records []HistoricalPrice
 	db, err := GetConnection()
+	defer db.Close()
 	if err != nil {
 		return records, err
 	}
 	queryF := `
     SELECT *
     FROM %s
-    WHERE date = '2022-01-25'
-    LIMIT 10
+    WHERE date >= '2021-01-01' and date < '2022-01-01'
 `
 
 	query := fmt.Sprintf(queryF, table)
@@ -72,17 +86,19 @@ func GetRecords(table string) ([]HistoricalPrice, error) {
 	for rows.Next() {
 		var (
 			r        HistoricalPrice
+			date     time.Time
 			id       int
 			volume   float64
 			turnover float64
 		)
-		err = rows.Scan(&id, &r.Date, &r.Ask, &r.Bid, &r.Open, &r.High, &r.Low, &r.Close, &volume, &turnover, &r.CodeF)
+		err = rows.Scan(&id, &date, &r.Ask, &r.Bid, &r.Open, &r.High, &r.Low, &r.Close, &volume, &turnover, &r.CodeF)
 		if err != nil {
 			return records, err
 		}
 		_ = id
-		// _ = turnover
-		// _ = volume
+		d := civil.DateOf(date.Round(0))
+		r.Date = d
+		r.Code, _ = convertCode(r.CodeF)
 		r.Turnover = int(turnover)
 		r.Volume = int(volume)
 		records = append(records, r)
@@ -123,4 +139,57 @@ func RecordExists(table, date string) (bool, error) {
 func PrettyPrint(i interface{}) string {
 	s, _ := json.MarshalIndent(i, "", "\t")
 	return string(s)
+}
+
+func chunkSlice(items []int, chunkSize int) [][]int {
+	var chunks [][]int
+	for chunkSize < len(items) {
+		chunks = append(chunks, items[0:chunkSize])
+		items = items[chunkSize:]
+	}
+	return append(chunks, items)
+}
+func makeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
+
+// insertRows demonstrates inserting data into a table using the streaming insert mechanism.
+func insertRows(records []HistoricalPrice) error {
+
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, "stock-lib")
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	inserter := client.Dataset("stock").Table("stock").Inserter()
+
+	var items []*HistoricalPrice
+	for i := range records {
+		items = append(items, &records[i])
+	}
+	if err := inserter.Put(ctx, items); err != nil {
+		return err
+	}
+	return nil
+}
+
+func convertCode(s string) (int, error) {
+
+	if strings.TrimSpace(s) == "" {
+		return 0, fmt.Errorf("Empty string")
+	}
+
+	ss := strings.TrimLeft(s, "0")
+	i, err := strconv.Atoi(ss)
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
 }
